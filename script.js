07b93df5fa3e471dd2e566a360d09e46fb1da7b3
pattern-screamer <schuurmanlukas@gmail.com>
2026-02-05 13:40:00 +0100

    Add change resolution button

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
+const resolution = document.querySelector('#resolution');
 const sketchpad = document.querySelector('.sketchpad');
 
-const side = 16;
-
-for (let i = 0; i < side; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  for (let i = 0; i < side; i++) {
-    const cell = document.createElement('div');
-    row.appendChild(cell);
-    cell.classList.add('cell');
-    cell.id = 'idlol';
-  }
-  addHoverEffect(row);
-  sketchpad.appendChild(row);
-}
+let side = 16;
 
 function addHoverEffect(row) {
-  row.addEventListener('mouseover', changeBG)
+  row.addEventListener('mouseover', colorCell);
 }
 
-function changeBG(event) {
-  event.target.style.backgroundColor = '#f00';
-  console.log(event.target.id);
+function wipeHoverEffect(row) {
+  row.addEventListener('mouseout', wipeCell);
 }
+
+function colorCell(event) {
+  event.target.style.backgroundColor = '#f00';
+}
+
+function wipeCell(event) {
+  event.target.style.backgroundColor = '#fff';
+}
+
+function drawGrid() {
+  sketchpad.innerHTML = '';
+  for (let i = 0; i < side; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.style.height = String(100 / side) + '%';
+    for (let i = 0; i < side; i++) {
+      const cell = document.createElement('div');
+      row.appendChild(cell);
+      cell.classList.add('cell');
+    }
+    addHoverEffect(row);
+    sketchpad.appendChild(row);
+  }
+}
+
+resolution.addEventListener('click', () => {
+  do {
+    side =  prompt("What should be the new height and width of the grid in cells? (MAX 100)", 16);
+  } while (side > 100)
+  drawGrid();
+});
+
+drawGrid();
